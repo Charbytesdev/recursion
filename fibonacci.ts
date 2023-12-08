eb27aab1adc1692490708a224ed4202a0bfccb94
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-12-08 12:02:00 +0200

    Implement fibonacci recursively

--- fibonacci.ts
+++ fibonacci.ts
@@ -13,4 +13,10 @@ function fibs(num: number) {
   return res;
 }
 
-console.log(fibs(13));
+function fibsRec(num: number): number {
+  if (num === 1) return 0;
+  else if (num === 2) return 1;
+  else return fibsRec(num - 1) + fibsRec(num - 2);
+}
+
+console.log(fibsRec(13));
